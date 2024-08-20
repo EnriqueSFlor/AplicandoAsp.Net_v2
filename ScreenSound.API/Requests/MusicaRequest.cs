7282dcb5061d44bbc746b1dbce14050a940810e8
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-08-19 21:26:33 -0300

    19082024

--- ScreenSound.API/Requests/MusicaRequest.cs
+++ ScreenSound.API/Requests/MusicaRequest.cs
@@ -2,6 +2,6 @@
 
 namespace ScreenSound.API.Requests
 {
-    public record MusicaRequest([Required] string nome, [Required] int ArtistaId, int anoLancamento, ICollection<GeneroRequest> Generos =null);
+    public record MusicaRequest([Required] string nome, [Required] int ArtistaId, int anoLancamento, ICollection<GeneroRequest> Generos = null);
    
 }
